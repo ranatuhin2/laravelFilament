9bd8ef6e6abcdb2cf1b30d594ee3daa27cf99683
Tuhin Rana Mondal <aimlesstuhin@gmail.com>
2025-06-21 23:15:43 +0530

    Added Filamanet Package

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::get('test', function(){
+    echo "Test Route!";
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
